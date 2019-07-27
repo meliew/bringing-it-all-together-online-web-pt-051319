18ad820c46eff2ae6434202f6b46ef855c1d08a8
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-27 17:28:01 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -88,7 +88,11 @@ class Dog
 new_dog
   end
 
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE name = ?
+    SQL
   end
 
   def update
