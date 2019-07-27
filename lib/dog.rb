7f5e3fef0dd3a26dd3ca247efdb62a7eaa2fac17
Melissa Williams <melissa@brandmeetscopy.com>
2019-07-27 17:29:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -99,6 +99,10 @@ new_dog
   end
 
   def update
+    sql = <<-SQL
+    UPDATE dogs SET name = ?, breed = ? WHERE id = ?
+    SQL
+     DB[:conn].execute(sql, self.name, self.breed, self.id)
   end
 
 
